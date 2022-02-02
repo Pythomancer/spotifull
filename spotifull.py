471e94dd8a1f3699001ff5656a9a27099240c09a
Pythomancer <austermannniklas@outlook.com>
2022-02-01 21:28:57 -0500

    added youtifull functionality

--- spotifull.py
+++ spotifull.py
@@ -1,5 +1,6 @@
 import os
 from urllib.parse import urlparse
+import urllib
 
 import discord
 
@@ -8,13 +9,15 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 from youtubesearchpython import VideosSearch
 
+import lxml
+from lxml import etree
 
 spotifyclientid = os.environ["CLIENT_ID"]
 spotifysecret = os.environ["CLIENT_SECRET"]
 
 auth_manager = SpotifyClientCredentials(
-    client_id = os.environ["CLIENT_ID"],
-    client_secret = os.environ["CLIENT_SECRET"]
+    client_id=os.environ["CLIENT_ID"],
+    client_secret=os.environ["CLIENT_SECRET"]
 )
 
 bot = discord.Client()
@@ -32,10 +35,18 @@ async def on_ready():
 async def on_message(message):
     if message.content == "cum":
         await message.channel.send("it works")
-    if "https://open.spotify.com/track" in message.content:
-        results = sp.track(urlparse(message.content).path[7:])
-        videosSearch = VideosSearch(
-            results["album"]["artists"][0]["name"] + " " + results["name"], limit=1)
-        await message.reply(videosSearch.result()["result"][0]["link"])
+    if(not(message.author.bot)):
+        if "https://open.spotify.com/track" in message.content:
+            results = sp.track(urlparse(message.content).path[7:])
+            videosSearch = VideosSearch(
+                results["album"]["artists"][0]["name"] + " " + results["name"], limit=1)
+            await message.reply(videosSearch.result()["result"][0]["link"])
+        elif "https://www.youtube.com/watch?v=" in message.content:
+            embeds = message.embeds
+            for embed in embeds:
+                title = embed.to_dict()['title']
+            results = sp.search(q='track:' + title, limit=1, type='track')
+            if len(results["tracks"]["items"]) > 0:
+                await message.reply(results["tracks"]["items"][0]["external_urls"]["spotify"])
 
 bot.run(os.environ["TOKEN"])
